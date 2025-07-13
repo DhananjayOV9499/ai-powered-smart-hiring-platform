3160b13969f7242e10c56e276979c75f069640b8
Harsh Gaur <gaurh6032@gmail.com>
2025-07-14 00:04:15 +0530

    Initial clean commit: added candidate-service

---
+++ candidate-service/src/test/java/com/aiplatform/candidateservice/CandidateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiplatform.candidateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
